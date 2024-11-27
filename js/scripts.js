a7282f8b4ad2d689be466053236f1282c63c2ced
mikethompson100 <mikethompson100@hotmail.com>
2024-11-27 09:42:10 -0500

    Fixed validation errors. Cleaned up code on modals.

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,5 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-document.getElementById('brazil').addEventListener('hidden.bs.modal', function () {
-    const iframe = document.getElementById('youtubeVideo');
-    iframe.src = iframe.src; // Reset the iframe src
- });
+
   
