aa05db4b0bf3d56bdd0c2e17d853b87e2be366fa
mikethompson100 <mikethompson100@hotmail.com>
2024-12-17 21:31:17 -0500

    Code formatted.

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Hamburger menu icons toggling in and out of view
-    const toggler = document.querySelector('.navbar .navbar-toggler');
-    const nav_logos = document.getElementById('nav-logos');
-    const navbarCollapse = document.querySelector('.navbar-collapse');
+const toggler = document.querySelector('.navbar .navbar-toggler');
+const nav_logos = document.getElementById('nav-logos');
+const navbarCollapse = document.querySelector('.navbar-collapse');
 
-    // Immediately hides the nav_logos element when the menu button is clicked
-    toggler.addEventListener('click', function() {
-        nav_logos.setAttribute("style", "display: none !important;");
-    });
+// Immediately hides the nav_logos element when the menu button is clicked
+toggler.addEventListener('click', function () {
+    nav_logos.setAttribute("style", "display: none !important;");
+});
 
-    // Waits for the collapse event to finish before displaying logos again
-    navbarCollapse.addEventListener('hidden.bs.collapse', function() {
-        nav_logos.setAttribute("style", "display: flex !important;");
-    });
+// Waits for the collapse event to finish before displaying logos again
+navbarCollapse.addEventListener('hidden.bs.collapse', function () {
+    nav_logos.setAttribute("style", "display: flex !important;");
+});
